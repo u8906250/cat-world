420dfd94454a7ae1c2086b1b1cfef6581d2a7653
u8906250 <u8906250@gmail.com>
2015-02-14 17:54:57 +0800

    remove OrbitCOntrols mouse event

--- catworld.js
+++ catworld.js
@@ -1,15 +1,15 @@
 var container;
-var camera, controls, scene, renderer;
+var camera, controls, scene, renderer, grid;
+var mouse2D = new THREE.Vector3( 0, 10000, 0.5 );
+var isMouseDown = false;
+var onMouseDownPosition = new THREE.Vector2(), onMouseDownPhi = 60, onMouseDownTheta = 45;
+var radius = 1600, theta = 90, phi = 60;
+var target = new THREE.Vector3( 0, 200, 0 );
 
 function render()
 {
+	camera.lookAt( target );
 	renderer.render( scene, camera );
-}
-
-function animate()
-{
-	requestAnimationFrame(animate);
-	controls.update();
 }
 
 function onWindowResize()
@@ -20,6 +20,40 @@ function onWindowResize()
 	render();
 }
 
+ function onDocumentMouseDown( event ) {
+    event.preventDefault();
+    isMouseDown = true;
+    onMouseDownTheta = theta;
+    onMouseDownPhi = phi;
+    onMouseDownPosition.x = event.clientX;
+    onMouseDownPosition.y = event.clientY;
+}
+function onDocumentMouseUp( event ) {
+	event.preventDefault();
+	isMouseDown = false;
+	onMouseDownPosition.x = event.clientX - onMouseDownPosition.x;
+	onMouseDownPosition.y = event.clientY - onMouseDownPosition.y;
+	if ( onMouseDownPosition.length() > 5 )
+		return;
+	render();
+}
+  function onDocumentMouseMove( event ) {
+    event.preventDefault();
+	
+    if (isMouseDown) {
+      theta = - ( ( event.clientX - onMouseDownPosition.x ) * 0.5 ) + onMouseDownTheta;
+      phi = ( ( event.clientY - onMouseDownPosition.y ) * 0.5 ) + onMouseDownPhi;
+      phi = Math.min( 180, Math.max( 0, phi ) );
+      camera.position.x = radius * Math.sin( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
+      camera.position.y = radius * Math.sin( phi * Math.PI / 360 );
+      camera.position.z = radius * Math.cos( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
+      camera.updateMatrix();
+    }
+    mouse2D.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse2D.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+	render();
+}
+
 function init()
 {
 	container = document.createElement( 'div' );
@@ -27,15 +61,13 @@ function init()
 	
 	scene = new THREE.Scene();
 	
-	camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
-	camera.position.set( 500, 800, 1300 );
-
-	controls = new THREE.OrbitControls( camera );
-	controls.damping = 0.2;
-	controls.addEventListener( 'change', render );
+	camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 )
+    camera.position.x = radius * Math.sin( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 )
+    camera.position.y = radius * Math.sin( phi * Math.PI / 360 )
+    camera.position.z = radius * Math.cos( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 )
 	
 	// grid
-	var size = 500, step = 30;
+	var size = 500, step = 50;
 	var geometry = new THREE.Geometry();
 	for ( var i = - size; i <= size; i += step ) {
 		geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
@@ -45,7 +77,10 @@ function init()
 	}
 	var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2, transparent: true } );
 	var line = new THREE.Line( geometry, material, THREE.LinePieces );
+	grid = line
 	scene.add( line );
+	
+	
 	
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setClearColor( 0xf0f0f0 );
@@ -53,8 +88,11 @@ function init()
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	container.appendChild( renderer.domElement );
 	
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+    document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+	
 	window.addEventListener( 'resize', onWindowResize, false );
-	animate();
 }
 
 function catworld()
